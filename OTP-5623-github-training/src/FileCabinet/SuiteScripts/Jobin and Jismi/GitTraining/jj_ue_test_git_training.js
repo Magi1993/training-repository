fc01f7930d580d2b2db994a6d4831cc1bd04d84b
Magi1993 <112927653+Magi1993@users.noreply.github.com>
2023-11-21 15:55:14 +0530

    OTP-5676-test-git-training

--- OTP-5623-github-training/src/FileCabinet/SuiteScripts/Jobin and Jismi/GitTraining/jj_ue_test_git_training.js
+++ OTP-5623-github-training/src/FileCabinet/SuiteScripts/Jobin and Jismi/GitTraining/jj_ue_test_git_training.js
@@ -7,15 +7,7 @@ define(['N/record'],
  * @param{record} record
  */
     (record) => {
-        /**
-         * Defines the function definition that is executed before record is loaded.
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.newRecord - New record
-         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-         * @param {Form} scriptContext.form - Current form
-         * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
-         * @since 2015.2
-         */
+        
         const beforeLoad = (scriptContext) => {
             let objRecord = record.load({
                 type:record.Type.SALES_ORDER,
